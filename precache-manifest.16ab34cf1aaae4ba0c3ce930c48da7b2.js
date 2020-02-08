d3ba366cf1167f59e201cb5a560d538d2a6a12e9
Adam Newhouser <adamnewhouser@gmail.com>
2020-02-08 16:55:24 -0500

    Updates

---
+++ precache-manifest.16ab34cf1aaae4ba0c3ce930c48da7b2.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "846afe711c6c0fadc0989531b30a2261",
+    "url": "/Final-Portfolio/index.html"
+  },
+  {
+    "revision": "2a53718222172ff641af",
+    "url": "/Final-Portfolio/static/css/main.81fbd1c2.chunk.css"
+  },
+  {
+    "revision": "ec18b0c575fd87373189",
+    "url": "/Final-Portfolio/static/js/2.591b5900.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/Final-Portfolio/static/js/2.591b5900.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a53718222172ff641af",
+    "url": "/Final-Portfolio/static/js/main.bbd67c21.chunk.js"
+  },
+  {
+    "revision": "7b289c5c2c3346df1270",
+    "url": "/Final-Portfolio/static/js/runtime-main.1c730e4a.js"
+  },
+  {
+    "revision": "fcd1669c95cf5877a458ae14946e3777",
+    "url": "/Final-Portfolio/static/media/adam_brady.fcd1669c.JPG"
+  },
+  {
+    "revision": "63f919d12c1d4a679cb398a3c2cb9492",
+    "url": "/Final-Portfolio/static/media/manageLazily.63f919d1.png"
+  },
+  {
+    "revision": "1e004afa8f3e1791bd509950818cee45",
+    "url": "/Final-Portfolio/static/media/space-quiz-project.1e004afa.png"
+  },
+  {
+    "revision": "3c2d6a3a315893a6ea9ce592410d6901",
+    "url": "/Final-Portfolio/static/media/spaced-repetition.3c2d6a3a.png"
+  }
+]);
